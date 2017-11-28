8598950d3b7990e1bf9e400e04204f6469b70485
123noob <zsjay2013@gmail.com>
2017-11-28 18:32:41 +0800

    add TCP connection

--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,9 @@
 from socket import * 
-serverName = 'hostname'
+serverName = 'localhost'
 serverPort = 12000
-# 第1个参数ipv4，第2个参数UDP
-clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = raw_input('Input lowercase sentence:')
 clientSocket.sendto(message, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage)
+print modifiedMessage
 clientSocket.close()
